202ab81c25121fcaab4fd051d5423415ff766db5
Colin Gilgenbach <colin@gilgenbach.net>
2020-11-17 15:00:28 -0500

    Updates to type parsing

--- src/Types.jl
+++ src/Types.jl
@@ -19,22 +19,13 @@ function output_type(::ParseType{T}) where {T} T end
 #                          FunctionWrapper{Result{T, String}, Tuple{String}}}
 
 to_parse_type(p::ParseType)::ParseType = p
-function to_parse_type(t::Type)::ParseType
-    if t == Bool
-        BoolType()
-    elseif t <: Number
-        NumType{t}()
-    else
-        TypeType{t}()
-    end
-end
-function to_parse_type(tup::Tuple{Vararg{Any}})::ParseType
-    mapped = map(to_parse_type, tup)
-    output_types = Tuple{map(output_type, mapped)...}
-    TupleType{output_types}(mapped)
-end
-function to_parse_type(f::FunctionWrapper{T, Tuple{String}})::FuncType{T} where {T} FuncType(f) end
-function to_parse_type(f::FunctionWrapper{Result{T, String}, Tuple{String}})::FuncType{T} where {T} FuncType(f) end
+to_parse_type(::Type{Bool})::BoolType = BoolType()
+to_parse_type(t::Type)::TypeType = TypeType{t}()
+to_parse_type(tup::Tuple{Vararg{Any}})::TupleType =  TupleType(map(to_parse_type, tup))
+function to_parse_type(::Type{T})::NumType where {T <: Number} NumType{T}() end
+function to_parse_type(f::Function)::FuncType FuncType(f) end
+#function to_parse_type(f::FunctionWrapper{T, Tuple{String}})::FuncType{T} where {T} FuncType(f) end
+#function to_parse_type(f::FunctionWrapper{Result{T, String}, Tuple{String}})::FuncType{T} where {T} FuncType(f) end
 
 struct BoolType <: ParseType{Bool} end
 function (::BoolType)(s::String)::Result{Bool, String}
@@ -59,8 +50,17 @@ function (c::ChoiceType{T})(s::String)::Result{T, String} where {T}
 end
 
 struct FuncType{T} <: ParseType{T}
-    f::Union{FunctionWrapper{T, Tuple{String}},
-             FunctionWrapper{Result{T, String}, Tuple{String}}}
+    f::FunctionWrapper{Result{T, String}, Tuple{String}}
+
+    function FuncType(f::Function)
+        if !hasmethod(f, Tuple{String})
+            error("Parse function not callable with type 'String'")
+        end
+        ret_types = Base.return_types(f, Tuple{String})
+        # strip result types from inference
+        T = Union{strip_result_type(ret_types)...}
+        new{T}(f)
+    end
 end
 function (f::FuncType{T})(s::String)::Result{T, String} where {T}
     catch_result(() -> f.f(s))
@@ -86,21 +86,39 @@ struct RangeType{T <: Number} <: ParseType{T}
 end
 function (r::RangeType{T})(s::String)::Result{T, String} where {T}
     num = @try_unwrap NumType{T}()(s)
-    if num < minimum(r.range) || num > maximum(r.range)
+    if !(minimum(r.range) <= num <= maximum(r.range))
         return Err("Number '$num' out of range $(minimum(r.range)):$(maximum(r.range))")
     end
     Ok(num)
 end
 
+function strip_result_type(ty::Union)::Array{Type}
+    collect(Iterators.flatten(map(strip_result_type, Base.uniontypes(ty))))
+end
+function strip_result_type(ty::Type{Ok{T}})::Array{Type} where {T}
+    [T]
+end
+function strip_result_type(::Type{Err{E}})::Array{Type} where {E}
+    []
+end
+strip_result_type(ty::Type)::Array{Type} = [ty]
+strip_result_type(a::Array)::Array{Type} = collect(Iterators.flatten(map(strip_result_type, a)))
+
 struct WrapType{T, U} <: ParseType{U}
     ty::ParseType{T}
     f::FunctionWrapper{Result{U, String}, Tuple{T}}
 
-    function WrapType(ty::ParseType{T}, f::FunctionWrapper{Result{U, String}, Tuple{T}}) where {T, U}
+    function WrapType(ty::ParseType{T}, f::Function) where {T}
+        if !hasmethod(f, Tuple{T})
+            error("Wrapping function not callable with type '$T'")
+        end
+        ret_types = Base.return_types(f, Tuple{T})
+        # strip result types from inference
+        U = Union{strip_result_type(ret_types)...}
         new{T, U}(ty, f)
     end
 end
-function WrapType(ty, f::FunctionWrapper{Result{U, String}, Tuple{Any}}) where {U}
+function WrapType(ty, f::Function) where {U}
     ty = to_parse_type(ty)
     WrapType{output_type(ty), U}(ty, f)
 end
@@ -111,6 +129,11 @@ end
 
 struct TupleType{T} <: ParseType{T}
     types::Tuple{Vararg{ParseType}}
+
+    function TupleType(types::Tuple{Vararg{ParseType}})
+        T = Tuple{map(output_type, types)...}
+        new{T}(types)
+    end
 end
 function (tup::TupleType{T})(stack::Array{String})::Result{T, <:Any} where {T}
     result = ()
